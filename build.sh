38d04880e2eceed0900bea337a5e3d5ad3cf6953
tom <townwang@retronix.com.tw>
2016-08-23 17:05:53 +0800

    modify build.sh Cross compile tools path define. default path is /opt/cross if not define CROSS_COMPILE_PATH.

--- build.sh
+++ build.sh
@@ -3,20 +3,32 @@
 set -e
 
 TOP=`pwd`
+export ARCH=arm
 
+# =====================================
+# Cross compile tools path define 
+# default path is /opt/cross if not define CROSS_COMPILE_PATH
+# =====================================
+#CROSS_COMPILE_PATH=/home/artie/JOB-Area/Android
+#CROSS_COMPILE_PATH=/opt/freescale/usr/local
+#CROSS_COMPILE_PATH=/media/tom/ext1t/freescale/cross-compile
 if [ -z $CROSS_COMPILE_PATH ];then
 CROSS_COMPILE_PATH=/opt/cross
 fi
-export ARCH=arm
 
+# =====================================
+# Cross compile tools version
+# =====================================
 #export CROSS_COMPILE=${CROSS_COMPILE_PATH}/gcc-4.8.5-glibc-2.23/arm-fsl-linux-gnueabi/bin/arm-fsl-linux-gnueabi-
-#export CROSS_COMPILE=${CROSS_COMPILE_PATH}/rtx-gcc-4.9.3-glibc-2.19-hf-64bits/arm-rtx-linux-gnueabihf/bin/arm-rtx-linux-gnueabihf-
+export CROSS_COMPILE=${CROSS_COMPILE_PATH}/rtx-gcc-4.9.3-glibc-2.19-hf-64bits/arm-rtx-linux-gnueabihf/bin/arm-rtx-linux-gnueabihf-
 #export CROSS_COMPILE=${CROSS_COMPILE_PATH}/rtx-gcc-4.9.3-glibc-2.23-hf-64bits/arm-rtx-linux-gnueabihf/bin/arm-rtx-linux-gnueabihf-
 #export CROSS_COMPILE=${CROSS_COMPILE_PATH}/rtx-gcc-5.3.0-glibc-2.23-hf/arm-rtx-linux-gnueabihf/bin/arm-rtx-linux-gnueabihf-
-#export CROSS_COMPILE=/home/artie/JOB-Area/Android/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
-export CROSS_COMPILE=${CROSS_COMPILE_PATH}/../freescale/usr/local/rtx-gcc-4.9.3-glibc-2.23-hf/arm-rtx-linux-gnueabihf/bin/arm-rtx-linux-gnueabihf- 
+#export CROSS_COMPILE=${CROSS_COMPILE_PATH}/prebuilts/gcc/linux-x86/arm/arm-eabi-4.6/bin/arm-eabi-
+#export CROSS_COMPILE=${CROSS_COMPILE_PATH}/rtx-gcc-4.9.3-glibc-2.23-hf/arm-rtx-linux-gnueabihf/bin/arm-rtx-linux-gnueabihf- 
 
-
+# =====================================
+# Configue file select
+# =====================================
 # A6 PLUS board
 #U_BOOT_DEFAULT_CONFIG=rtx_a6plus_mx6q_micro1g_null_roymark_tv_android_defconfig
 #U_BOOT_DEFAULT_CONFIG=rtx_a6plus_mx6q_micro1g_null_roymark_box_android_defconfig
